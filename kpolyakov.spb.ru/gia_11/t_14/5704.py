b75a8e0939bb9f31bbfbf50df3461dfc6422d2dc
koka-land <81404483+koka-land@users.noreply.github.com>
2023-03-01 19:08:07 +0300

    Update 5704.py

--- kpolyakov.spb.ru/gia_11/t_14/5704.py
+++ kpolyakov.spb.ru/gia_11/t_14/5704.py
@@ -1,3 +1,7 @@
+'''
+https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=5704
+'''
+
 for x in range(0, 130):
     a = 2 * 130 ** 4 + 3 * 130 ** 3 + x * 130 ** 2 + 3 * 130 + 2
     b = 3 * 130 ** 4 + x * 130 ** 3 + 2 * 130 ** 2 + 5 * 130 + 3
