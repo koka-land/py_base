3a8cd96ed9e1e2f67726c0f627090bd1df996100
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-06 15:20:06 +0300

    Update 4870.py

--- kpolyakov.spb.ru/gia_11/t_15/4870.py
+++ kpolyakov.spb.ru/gia_11/t_15/4870.py
@@ -2,14 +2,16 @@
 https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=4870
 '''
 
-print('Таблица истинности')
+#Таблица истинности
+
 print('P Q A F')
 for P in range(2):
     for Q in range(2):
         for A in range(2):
             print (P, Q, A, (not(A) <= (not(P))) <= (A <= Q))
 
-print('Решение')
+#Решение
+            
 def F(a, b, x):
     if a <= x <= b:
         return True
@@ -17,10 +19,12 @@ def F(a, b, x):
         return False
 
 mn = 10**10
+
 c = []
 
 for a in  range(25, 50):
     for b in  range(a+1, 51):
         if all((not(F(a, b, x)) <= (not(F(25, 50, x)))) <= (F(a, b, x) <= F(32, 47, x)) for x in range(1, 100)):
             c.append(b - a)
+            
 print(max(c))
