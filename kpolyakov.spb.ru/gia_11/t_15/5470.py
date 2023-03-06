4eb675b469710a290cf70cdf6a0e267ccbfc2f18
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-06 14:12:13 +0300

    Update 06.03

--- kpolyakov.spb.ru/gia_11/t_15/5470.py
+++ kpolyakov.spb.ru/gia_11/t_15/5470.py
@@ -14,11 +14,23 @@ Q = set(range(410, 824))
 A = set()
 k = 0
 
-print('Решение')
+print('Решение №1')
 
 for x in range(250, 830):
-    if not (((x in P) and (x not in A)) <= (x in Q)):
+    if not(((x in P) and (x not in A)) <= (x in Q)):
         A.add(x)
         k += 1
-
 print(k)
+
+print('Решение №2')
+c=[]
+def f(x,y,z):
+    if x<=y<=z:
+        return True
+    else:
+        return False
+for a in range(254,823):
+    for b in range(a+1,824):
+        if all((f(254,x,800) and (not(f(a,x,b))))<=f(410,x,823) for x in range(200,1000)):
+            c.append(b-a)
+print(min(c) + 1 ) #+1 пишем, потому что число x может быть нецелым, и нужно расширить отрезок на 1 точку вправо, невключая ее
