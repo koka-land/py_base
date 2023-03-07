6af0597648c33590664c2d26d79a2651161fb493
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-07 12:21:18 +0300

    update 07.03

--- kpolyakov.spb.ru/gia_11/t_02/6074.py
+++ kpolyakov.spb.ru/gia_11/t_02/6074.py
@@ -4,10 +4,8 @@ https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=6074
 
 from itertools import *
 
-
 def f(x, y, w, z):
     return (w == y) or ((x or z) and (z or y))
-
 
 for a1, a2, a3, a4, a5, a6, a7, a8 in product([0, 1], repeat=8):
     table = [(a1, 1, 1, a2), (a3, a4, 1, a5), (1, a6, a7, a8)]
