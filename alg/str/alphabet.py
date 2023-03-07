d839bc5d06380005c2a3d6f2cc3bb6ddecf15546
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-08 01:25:07 +0300

    Update 08.03

--- alg/str/alphabet.py
+++ alg/str/alphabet.py
@@ -1,6 +1,17 @@
+#1 способ
+
 s = ''
 
-for i in range(ord('A'), ord('Z')):
+for i in range(ord('A'), ord('Z') + 1):
     s = s + chr(i)
 
 print(s)
+
+#2 способ
+
+import string
+
+s = string.ascii_uppercase
+
+print(s)
+
