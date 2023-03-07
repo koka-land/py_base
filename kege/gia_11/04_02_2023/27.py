b68836b59d41d6d012e468e7ebdd4edc776fd7b5
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-08 01:37:07 +0300

    Update 08.03

--- kege/gia_11/04_02_2023/27.py
+++ kege/gia_11/04_02_2023/27.py
@@ -1,10 +1,18 @@
-f = open('files/27_A.txt', 'r')
+f = open('files/27_B.txt', 'r')
 sum = 0
+min = 10 ** 10
 
 n = f.readline()
 
 for i in f:
     sp = sorted(list(map(int, i.split())))
     sum = sum + sp[2]
+    if ((sp[2] - sp[0]) < min) and ((sp[2] - sp[0]) % 91 != 0):
+        min = sp[2] - sp[0]
+    if ((sp[2] - sp[1]) < min) and ((sp[2] - sp[1]) % 91 != 0):
+        min = sp[2] - sp[1]
 
-print(sum, sum % 91)
+if sum % 91 != 0:
+    print(sum)
+else:
+    print(sum - min)
