afd765a0c89065a2cb8d2552ba11faa952e36f3d
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-07 15:33:42 +0300

    Update 02.py

--- kege/gia_11/04_02_2023/02.py
+++ kege/gia_11/04_02_2023/02.py
@@ -1,9 +1,7 @@
 from itertools import *
 
-
 def f(x, y, w, z):
     return not ((w or not (y)) and x) or (y == z)
-
 
 for a1, a2, a3, a4 in product([0, 1], repeat=4):
     table = [(0, 1, a1, 0), (1, 1, 0, a2), (a3, 1, a4, 0)]
