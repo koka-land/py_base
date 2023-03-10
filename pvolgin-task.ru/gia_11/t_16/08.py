ace0594d4bb9bbca9f60994990ffec95df23a206
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-10 13:51:38 +0300

    Update 10.03

---
+++ pvolgin-task.ru/gia_11/t_16/08.py
@@ -0,0 +1,28 @@
+def prime(x):
+    f = 0
+    for i in range(2, int(x**(1/2)) + 1):
+        if x % i == 0:
+            f = 1
+            break
+    if f == 0:
+        return True
+    else:
+        return False
+
+def sum_num(x):
+    return sum(map(int, str(x)))
+def f(n):
+    if n <= 1:
+        return 1
+    if (n % 2 == 0) and (n > 1):
+        return 11 * n + f(n - 1)
+    if (n % 2 == 1) and (n > 1):
+        return f(n - 2) * 11 + n
+
+count = 0
+
+for i in range(2, 36):
+    if prime(sum_num(f(i))):
+        count += 1
+
+print(count)
