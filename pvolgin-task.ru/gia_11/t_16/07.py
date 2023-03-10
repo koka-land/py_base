c7c7ecc887c7c7171f32739ec9bb0db1cb67ccde
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-10 13:43:39 +0300

    Update 10.03

---
+++ pvolgin-task.ru/gia_11/t_16/07.py
@@ -0,0 +1,24 @@
+def prime(x):
+    f = 0
+    for i in range(2, int(x**(1/2)) + 1):
+        if x % i == 0:
+            f = 1
+            break
+    if f == 0:
+        return True
+    else:
+        return False
+def f(n):
+    if n == 0:
+        return 1
+    else:
+        return 7 * (n - 1) + f(n - 1)
+
+count = 0
+
+for i in range(1, 201):
+    if prime(f(i)) == True:
+        count += 1
+        #print(i, f(i))
+
+print(count)
