8657ac2bf9927c4351c4ef795efc78fb822649d3
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-12 00:41:51 +0300

    Update 12.03

---
+++ kpolyakov.spb.ru/gia_11/t_02/6111.py
@@ -0,0 +1,16 @@
+'''
+https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=6111
+*by Aldoshin Nikita
+'''
+
+from itertools import *
+
+def f(x, y, w, z):
+    return w  or (y <= z) and x
+
+for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
+    table = [(a1, a2, 0, 0), (1, 1, a3, a4), (1, a5, a6, 1)]
+    if len(table) == len(set(table)):
+        for p in permutations('xywz'):
+            if [f(**dict(zip(p, r))) for r in table] == [1, 0, 0]:
+                print(p)
