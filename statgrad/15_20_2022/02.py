6a3b0981c0b0d226e6b623f180cece7b8ed71e13
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-13 16:28:32 +0300

    Update 13.03

--- statgrad/15_20_2022/02.py
+++ statgrad/15_20_2022/02.py
@@ -1,9 +1,7 @@
 from itertools import *
 
-
 def f(x, y, w, z):
     return (x == (not (y))) <= ((z <= (not (w))) and (w <= y))
-
 
 for a1, a2, a3, a4, a5 in product([0, 1], repeat=5):
     table = [(1, 1, 0, 1), (0, a1, 0, a2), (a3, a4, a5, 0)]
