a94265b78c6923e4299c397579c4e525840bcc3e
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 18:49:01 +0300

    Update 29109.py

--- inf-ege.sdamgia.ru/gia_11/t_02/29109.py
+++ inf-ege.sdamgia.ru/gia_11/t_02/29109.py
@@ -8,7 +8,7 @@ from itertools import *
 def f(x, y, z, w):
     return ((z <= w) or (y == w)) and ((x or z) == y)
 for a1, a2, a3, a4 in product([0, 1], repeat=4):
-    table=[(0, 1, 1, 0), (a1, 1, 0, a2), (0, a3, a4, 1)]
+    table = [(0, 1, 1, 0), (a1, 1, 0, a2), (0, a3, a4, 1)]
     if len(table) == len(set(table)):
         for p in permutations('xyzw'):
             if [f(**dict(zip(p, r))) for r in table] == [1, 1, 1]:
