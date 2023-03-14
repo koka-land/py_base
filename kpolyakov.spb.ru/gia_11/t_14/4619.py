cc1d6cc6dc0be8359c02aaa5ae1e2d6b8d2fc96d
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:06:57 +0300

    Update 4619.py

--- kpolyakov.spb.ru/gia_11/t_14/4619.py
+++ kpolyakov.spb.ru/gia_11/t_14/4619.py
@@ -2,7 +2,7 @@
 https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=4619
 '''
 
-a = 11*15**65 + 18*15**38 + 14*15**17 + 19*15**11 + 18338
+a = 11 * 15**65 + 18 * 15**38 + 14 * 15**17 + 19 * 15**11 + 18338
 sp = []
 
 while a > 0:
