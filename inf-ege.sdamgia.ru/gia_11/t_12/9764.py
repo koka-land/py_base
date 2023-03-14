7e658fbe8ec0907790e7f28c63d69d59fb857408
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-14 20:15:00 +0300

    Update 14.03

--- inf-ege.sdamgia.ru/gia_11/t_12/9764.py
+++ inf-ege.sdamgia.ru/gia_11/t_12/9764.py
@@ -3,11 +3,12 @@ https://inf-ege.sdamgia.ru/problem?id=9764
 *by Aldoshin Nikita
 '''
 
+s = '9' * 127
 
-s='9'*127
-while ('333'in s) or ('999'in s):
-    if ('333'in s):
-        s=s.replace('333','9',1)
+while ('333' in s) or ('999' in s):
+    if ('333' in s):
+        s = s.replace('333', '9', 1)
     else:
-        s=s.replace('999','3',1)
+        s = s.replace('999', '3', 1)
+
 print(s)
