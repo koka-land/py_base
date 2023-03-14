d1aa97ba7d53b942561e41346a6afb42781428be
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-14 21:19:46 +0300

    Update 14.03

--- inf-ege.sdamgia.ru/gia_11/t_17/37340.py
+++ inf-ege.sdamgia.ru/gia_11/t_17/37340.py
@@ -6,10 +6,10 @@ https://inf-ege.sdamgia.ru/problem?id=37340
 f = open('files/17_37340.txt')
 c = 0
 m = 0
-l = [int(i)for i in f]
+l = [int(i) for i in f]
 
 for i in range(len(l) - 1):
-    for j in range (i + 1, len(l)):
+    for j in range(i + 1, len(l)):
         if (l[i] - l[j]) % 2 == 0 and (l[i] % 31 == 0 or l[j] % 31 == 0):
             c += 1
             m = max(m, l[i] + l[j])
