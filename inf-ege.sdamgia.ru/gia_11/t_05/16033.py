c2111355540103f3c51166a93677a92159d855e7
Cheese Depression <karantin.serb@yandex.ru>
2023-03-14 22:23:06 +0300

    Update 14.03

---
+++ inf-ege.sdamgia.ru/gia_11/t_05/16033.py
@@ -0,0 +1,17 @@
+'''
+https://inf-ege.sdamgia.ru/problem?id=16033
+*by Aldoshin Nikita
+'''
+
+
+for i in range(0,200):
+    b = bin(i)[2::]
+    b = str(b)
+    if i % 2 == 0:
+        b = b + '01'
+    else:
+        b = b + '10'
+    r = int(b,2)
+    if r > 102:
+        print(r)
+        break
