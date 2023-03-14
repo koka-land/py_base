c3cedf7c83eb5190996a0f5e89b40e31fcb2d509
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:16:47 +0300

    Update 33757.py

--- inf-ege.sdamgia.ru/gia_11/t_12/33757.py
+++ inf-ege.sdamgia.ru/gia_11/t_12/33757.py
@@ -3,13 +3,13 @@ https://inf-ege.sdamgia.ru/problem?id=33757
 *by Aldoshin Nikita
 '''
 
-for a in range(0,60):
-    for b in range(0,60):
-        for c in range(0,60):
-            s='0'+a*'1'+b*'2'+c*'3'
+for a in range(0, 60):
+    for b in range(0, 60):
+        for c in range(0, 60):
+            s = '0' + a * '1' + b * '2' + c * '3'
             while '01' in s or '02' in s or '03' in s:
-                s=s.replace('01','30',1)
-                s=s.replace('02','101',1)
-                s=s.replace('03','202',1)
-            if s.count('1')==20 and s.count('2')==10 and s.count('3')==70:
+                s = s.replace('01', '30', 1)
+                s = s.replace('02', '101', 1)
+                s = s.replace('03', '202', 1)
+            if s.count('1') == 20 and s.count('2') == 10 and s.count('3') == 70:
                 print(a)
