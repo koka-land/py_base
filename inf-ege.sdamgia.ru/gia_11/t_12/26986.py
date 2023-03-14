f8e37c4fb233e27e7b9a348b0e7c5911f0fec910
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:17:56 +0300

    Update 26986.py

--- inf-ege.sdamgia.ru/gia_11/t_12/26986.py
+++ inf-ege.sdamgia.ru/gia_11/t_12/26986.py
@@ -5,12 +5,12 @@ https://inf-ege.sdamgia.ru/problem?id=26986
 
 s = '7' * 40 + '9' * 40 + '4' * 50
 
-while ('49' in s)or ('97'in s)or('47'in s):
+while ('49' in s) or ('97' in s) or ('47' in s):
     if ('47' in s):
-        s=s.replace('47','74',1)
+        s = s.replace('47', '74', 1)
     if ('97' in s):
-        s=s.replace('97','79',1)
+        s = s.replace('97', '79', 1)
     if ('49' in s):
-        s=s.replace('49','94',1)
+        s = s.replace('49', '94', 1)
 
-print(s[25],s[71],s[105])
+print(s[25], s[71], s[105])
