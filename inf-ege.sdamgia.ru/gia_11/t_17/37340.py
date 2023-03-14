ce40a7771a7107ba28472f37cedae4928d042991
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-14 21:19:21 +0300

    Update 14.03

--- inf-ege.sdamgia.ru/gia_11/t_17/37340.py
+++ inf-ege.sdamgia.ru/gia_11/t_17/37340.py
@@ -3,17 +3,16 @@ https://inf-ege.sdamgia.ru/problem?id=37340
 *by Aldoshin Nikita
 '''
 
-
 f = open('files/17_37340.txt')
 c = 0
 m = 0
 l = [int(i)for i in f]
 
-for i in range (len (l) -1):
+for i in range(len(l) - 1):
     for j in range (i + 1, len(l)):
-        if (l[i]-l[j]) %2 == 0 and (l[i] % 31 == 0 or l[j] % 31 == 0):
+        if (l[i] - l[j]) % 2 == 0 and (l[i] % 31 == 0 or l[j] % 31 == 0):
             c += 1
-            m=max(m,l[i]+l[j])
+            m = max(m, l[i] + l[j])
 
-print (c,m)
+print(c, m)
 
