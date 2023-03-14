7fabda1cefdbb8fb3d23c3a9989bc73d2aab6945
Cheese Depression <karantin.serb@yandex.ru>
2023-03-14 21:47:53 +0300

    Update 14.03

---
+++ inf-ege.sdamgia.ru/gia_11/t_16/4644.py
@@ -0,0 +1,11 @@
+'''
+https://inf-ege.sdamgia.ru/problem?id=4978
+*by Aldoshin Nikita
+'''
+
+def f(x):
+    if x == 1:
+        return 1
+    if x > 1:
+        return  f(x - 1) * f(x - 1) - f(x - 1) * x + 2 * x
+print(f(4))
