8c0610705dcffeca8c1c9f1dacba8647532a4920
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:07:10 +0300

    Update 4886.py

--- kpolyakov.spb.ru/gia_11/t_14/4886.py
+++ kpolyakov.spb.ru/gia_11/t_14/4886.py
@@ -2,7 +2,7 @@
 https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=4886
 '''
 
-a = 8 ** 888 + 16 * 16 ** 1616 - 2 ** 444
+a = 8**888 + 16 * 16**1616 - 2**444
 sp = []
 
 while a > 0:
