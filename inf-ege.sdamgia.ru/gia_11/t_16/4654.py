1f70708ec694ffda8a103c46d7d953a8d58240ea
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 18:44:13 +0300

    Update 14.03

--- inf-ege.sdamgia.ru/gia_11/t_16/4654.py
+++ inf-ege.sdamgia.ru/gia_11/t_16/4654.py
@@ -4,12 +4,13 @@ https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=6111
 '''
 
 def f(x):
-    if x==1:
+    if x == 1:
         return 1
-    if x==2:
+    if x == 2:
         return 1
-    if x==3:
+    if x == 3:
         return 1
-    if x>3:
-        return f(x-3)+f(x-2)
+    if x > 3:
+        return f(x-3) + f(x-2)
+    
 print(f(10))
