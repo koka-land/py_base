60d0b0afc72024eb1a19333682cf3e2d73185658
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:01:09 +0300

    Update 18570.py

--- inf-ege.sdamgia.ru/gia_11/t_23/18570.py
+++ inf-ege.sdamgia.ru/gia_11/t_23/18570.py
@@ -3,12 +3,12 @@ https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=6111
 *by Aldoshin Nikita
 '''
 
-
-def f(x,y):
-    if x>y or x==16:
+def f(x, y):
+    if x > y or x == 16:
         return 0
-    if x==y:
+    if x == y:
         return 1
     else:
-        return f(x+1,y)+f(x*2,y)+f(x*3,y)
-print(f(1,14)*f(14,50))
+        return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
+
+print(f(1, 14) * f(14, 50))
