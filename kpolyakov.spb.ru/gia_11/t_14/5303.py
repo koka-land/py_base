7418b5cee1090efea106e73d1b254a6bd99750af
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 20:06:09 +0300

    Update 5303.py

--- kpolyakov.spb.ru/gia_11/t_14/5303.py
+++ kpolyakov.spb.ru/gia_11/t_14/5303.py
@@ -2,7 +2,7 @@
 https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=5303
 '''
 
-a = abs(3*5**1984 - 7*25**777 - 11*125**666 - 404)
+a = abs(3 * 5 ** 1984 - 7 * 25 ** 777 - 11 * 125 ** 666 - 404)
 count = 0
 
 while a > 0:
