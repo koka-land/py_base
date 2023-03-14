140c0ba5c383220ba0bcb39e3ff8188530c9b738
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-14 18:49:33 +0300

    Update 18550.py

--- inf-ege.sdamgia.ru/gia_11/t_02/18550.py
+++ inf-ege.sdamgia.ru/gia_11/t_02/18550.py
@@ -7,6 +7,7 @@ from itertools import *
 
 def f(x, y, z, w):
     return ((y <= z) or (not x and w)) == (w == z)
+
 for a1, a2, a3 in product([0, 1], repeat=3):
     table = [(a1, 1, 0, 0), (0, 0, 0, 1), (0, 1, a2, a3)]
     if len(table) == len(set(table)):
