1e688ae5d9963d4c0cb47d126848c3133e070bd8
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-15 14:59:52 +0300

    Update 15.03

--- kege/gia_11/10_03_2023/27.py
+++ kege/gia_11/10_03_2023/27.py
@@ -1,19 +1,21 @@
 import math
 
-f = open('files/27_A_6760.txt', 'r')
+f = open('files/27_B_6760.txt', 'r')
 n = int(f.readline())
-min = 10**10
+price = [0] * n
+ds = us = 0
 
 sp = [list(map(int, i.split())) for i in f]
 
-for j in range(n):
-    lab = sp[0][0]
-    sum = 0
-    for i in range(1, n):
-        sum += abs(sp[i][0] - lab) * math.ceil(sp[i][1] / 48)
-    if sum < min:
-        min = sum
-    sp.append(sp[0])
-    sp.pop(0)
+for i in range(n):
+    sp[i][1] = math.ceil(sp[i][1] / 48)
+    if i > 0:
+        price[0] += (sp[i][0] - sp[0][0]) * sp[i][1]
+        ds += sp[i][1]
 
-print(min)
+for i in range(1, n):
+    us += sp[i-1][1]
+    price[i] = price[i-1] - ds * (sp[i][0] - sp[i-1][0]) + us * (sp[i][0] - sp[i-1][0])
+    ds -= sp[i][1]
+
+print(min(price))
