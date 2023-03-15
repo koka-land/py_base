6936ee9a47d9089a2ff650c97ca392e7bd2ed2d3
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-16 00:57:19 +0300

    Update 26957.py

--- inf-ege.sdamgia.ru/gia_11/t_12/26957.py
+++ inf-ege.sdamgia.ru/gia_11/t_12/26957.py
@@ -4,6 +4,7 @@ https://inf-ege.sdamgia.ru/problem?id=26957
 '''
 
 s = '>' + '1'*26 + '2'*10 + '3'*14
+
 while ('>' in s) or ('>2' in s) or ('>3' in s):
     if '>1' in s:
         s = s.replace('>1', '22>', 1)
@@ -11,5 +12,6 @@ while ('>' in s) or ('>2' in s) or ('>3' in s):
         s = s.replace('>2', '2>' , 1)
     if '>3' in s:
         s = s.replace('>3', '1>' , 1)
-    print(s.count('1')+s.count('2')*2)
+
+print(s.count('1') + s.count('2')*2)
 
