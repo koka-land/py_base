a5ab29e8e2b19625ffe6e91009751a342848af13
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-16 14:21:06 +0300

    Update 3206.py

--- inf-ege.sdamgia.ru/gia_11/t_08/3206.py
+++ inf-ege.sdamgia.ru/gia_11/t_08/3206.py
@@ -3,12 +3,14 @@ https://inf-ege.sdamgia.ru/problem?id=3206
 '''
 
 from itertools import product
+
 k = 0
-for x in product('АКРУ', repeat = 5):
+
+for x in product('АКРУ', repeat=5):
     s = ''.join(x)
     if s[0] == 'К':
         k += 1
-        print(s, k)
+        print(k, s)
 '''
 После запуска программы мы делаем вывод,что количество всех символов равно 1024,
 следовательно 257 элемент это и будет слово которое начинается с буквы К
