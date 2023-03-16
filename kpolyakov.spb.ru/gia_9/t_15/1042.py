c1e8c524b67328b7a59dba954d38c8b60efd437f
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-03-16 12:10:28 +0300

    Update 16.03

--- kpolyakov.spb.ru/gia_9/t_15/1042.py
+++ kpolyakov.spb.ru/gia_9/t_15/1042.py
@@ -3,11 +3,11 @@ https://kpolyakov.spb.ru/school/oge/gen.php?action=viewTopic&topicId=1042
 '''
 
 a = 1
-count = 0
+summ = 0
 
 while a > 0:
     a = int(input())
-    if (a >= 100) and( a <= 999) and (a %4 ==0):
-        count += a
+    if (a >= 100) and( a <= 999) and (a % 4 == 0):
+        summ += a
 
-print(count)
+print(summ)
