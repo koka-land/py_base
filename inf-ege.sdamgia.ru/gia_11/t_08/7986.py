70b5a7743ff2dfa66b153ea5095e7cb731114fc6
Cheese Depression <karantin.serb@yandex.ru>
2023-03-16 14:11:51 +0300

    Update 16.03

---
+++ inf-ege.sdamgia.ru/gia_11/t_08/7986.py
@@ -0,0 +1,12 @@
+'''
+https://inf-ege.sdamgia.ru/problem?id=7986
+'''
+
+from itertools import product
+
+k = 0
+for x in product('ЗИМА', repeat=5):
+    s = ''.join(x)
+    if s[0] in 'ЗМ' and s[-1] in 'ИА':
+        k += 1
+        print(s, k)
