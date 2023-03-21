3c35b269435b85ee4f87e671202b9270ad62ab5c
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-21 10:56:56 +0300

    Update 21.03

--- classworks/22_23/7/7j/for_1.py
+++ classworks/22_23/7/7j/for_1.py
@@ -3,7 +3,9 @@
 '''
 n = int(input('Введите количество чисел: '))
 summ = 0
+proiz = 1
 for i in range(n):
     a = int(input('Введите число: '))
     summ += a
+    proiz *= a
 print('Сумма:', summ)
