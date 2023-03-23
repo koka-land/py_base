f0dd9bd2deed316bcdc571c5bdbef695aa5f1b51
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-23 11:08:30 +0300

    Update 23.03

--- kege/gia_11/imc_spb/26.py
+++ kege/gia_11/imc_spb/26.py
@@ -1,7 +1,9 @@
 f = open('files/26_6641.txt', 'r')
 sp = []
 sp_s = []
+sp_check = []
 count = 0
+items = 0
 
 n, m = f.readline().split()
 n = int(n)
@@ -18,19 +20,27 @@ for i in sp:
 sp.sort()
 sp_s.sort()
 
-print(sp_s)
-
 for i in sp:
-    if i in sp_s:
-        sp_s.remove(i)
     if m - i[0] > 0:
+        items += 1
+        if i in sp_s:
+            sp_s.remove(i)
+        sp_check.append(i)
         m = m - i[0]
         if i[1] == 'S':
             count += 1
     else:
         break
 
-print(count)
-print(sp_s)
+sp_check.sort(reverse=True)
 
+for i in sp_check:
+    if i[1] == 'W':
+        if m + i[0] - sp_s[0][0] > 0:
+            m = m + i[0] - sp_s[0][0]
+            count += 1
+            sp_s.pop(0)
+        else:
+            break
 
+print(count, m)
