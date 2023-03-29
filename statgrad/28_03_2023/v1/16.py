b161d8c72a04283b4bbe1ed3284de2eb63ad39c3
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-29 19:37:15 +0300

    Update 29.03

--- statgrad/28_03_2023/v1/16.py
+++ statgrad/28_03_2023/v1/16.py
@@ -6,13 +6,25 @@ def f(a, b):
     if a < b:
         return f(b, a)
 
+for i in range(1, 100):
+    if f(i, 14) == 1:
+        print(i)
+
 count = 0
 start = 123456795
 stop = 1234567888
 
-for n in range (0, 14):
+for n in range(0, 14):
+    #print(n, f(n, 14))
     if f(n, 14) == 1:
         count += 1
 
 print((stop - start) // 14 * count)
 
+# Решение без функции
+
+count = 0
+for i in range(start, stop + 1, 2):
+    if i % 7 != 0:
+        count += 1
+print(count)
