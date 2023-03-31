4d94312bda2d0d07ec3a0727b2dae07564e8ff96
Aleksandr Ermakov <koka-land@yandex.ru>
2023-03-31 15:41:38 +0300

    Update 31.03

---
+++ statgrad/28_03_2023/v1/22.py
@@ -0,0 +1,16 @@
+f = open('../files/22.csv')
+
+sp = [0] + [list(map(int, i.split(';'))) for i in f]
+time = [0 for i in range(len(sp))]
+
+for i in range(1, len(sp)):
+    if sp[i][2] == 0:
+        time[i] = sp[i][1]
+    else:
+        sp_time = []
+        for j in range(2, len(sp[i])):
+
+        time[i] = max(sp_time)
+
+print(len(sp), sp)
+print(len(time), time)
