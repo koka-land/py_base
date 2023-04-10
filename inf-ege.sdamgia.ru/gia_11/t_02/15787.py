bc6c1d073a90b846d46b1c5975c8328343deb924
Aleksandr Ermakov <81404483+koka-land@users.noreply.github.com>
2023-04-11 01:42:50 +0300

    Update 15787.py

--- inf-ege.sdamgia.ru/gia_11/t_02/15787.py
+++ inf-ege.sdamgia.ru/gia_11/t_02/15787.py
@@ -12,5 +12,5 @@ for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
     table = [(1, a1, a2, 1), (1, a3, a4, a5), (a6, 1, a7, 1)]
     if len(table) == len(set(table)):
         for p in permutations('xyzw'):
-            if [f(**dict(zip(p, r)))for r in table] == [0,0,0]:
+            if [f(**dict(zip(p, r)))for r in table] == [0, 0, 0]:
                 print(*p)
