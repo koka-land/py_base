a20eec74c1aafddbff4756b9ecea3d748afed46a
Aleksandr Ermakov <koka-land@yandex.ru>
2023-06-12 20:59:32 +0300

    Update 12.06

---
+++ kompege/gia_11/tasks/t_27/8513-A.py
@@ -0,0 +1,12 @@
+f = open('files/27_A_8513.txt')
+k = int(f.readline())
+n = int(f.readline())
+sp = []
+for i in f:
+    sp.append(int(i))
+max_sum = 0
+for i in range(n - 4):
+    for j in range(i + 4, n - 1):
+        max_sum = max(max_sum, sp[i] + sp[j])
+
+print(max_sum)
