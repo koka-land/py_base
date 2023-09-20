821173803d9096466259d4dfcfbea6c510de886f
Aleksandr Ermakov <koka-land@yandex.ru>
2023-09-20 14:14:58 +0300

    Update: 20.09.23

---
+++ kpolyakov.spb.ru/gia_11/t_23/6779.py
@@ -0,0 +1,18 @@
+'''
+https://kpolyakov.spb.ru/school/ege/gen.php?action=viewTopic&topicId=6111
+*by Kuzmin Andrei
+'''
+
+def f(st,fin):
+    if st == fin:
+        return 1
+    elif st < fin:
+        return 0
+    elif (st == 9):
+        return 0
+    elif (st == 16):
+        return 0
+    else:
+        return f(st - 1, fin) + f(st - 2, fin) + f(st // 3, fin)
+
+print(f(19,3))
