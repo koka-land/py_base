86da930231ae0dd9ca33711e5a1751d274199cbf
Aleksandr Ermakov <koka-land@yandex.ru>
2023-10-03 11:46:13 +0300

    Update: 03.10.23

--- kpolyakov.spb.ru/gia_11/t_08/6719.py
+++ kpolyakov.spb.ru/gia_11/t_08/6719.py
@@ -8,10 +8,9 @@ s = 'АГМНСТУ'
 nomer = 0
 
 for c1, c2, c3, c4, c5, c6 in product(s, repeat=6):
-    if c1 != 'У':
-        slovo = c1 + c2 + c3 + c4 + c5 + c6
-        nomer += 1
-        if slovo.count('М') == 2 and slovo.count('Г') <= 1:
-            ans = nomer
+    slovo = c1 + c2 + c3 + c4 + c5 + c6
+    nomer += 1
+    if slovo.count('М') == 2 and slovo.count('Г') <= 1 and slovo[0] != 'У':
+        ans = nomer
 
 print(ans)
