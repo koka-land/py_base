0ef640d736107bf18950e9ef8f6a714a1cef9de1
Aleksandr Ermakov <koka-land@yandex.ru>
2023-10-13 15:32:06 +0300

    Update: 13.10.23

--- kompege/gia_11/tasks/t_14/7702.py
+++ kompege/gia_11/tasks/t_14/7702.py
@@ -1,10 +1,9 @@
 sp = set()
 
-for x in range(18):
-    for y in range(9, 18):
-        if x < y:
-            a = 5 * 18**3 + x * 18**2 + y * 18**1 + 10
-            b = 1 * y**3 + 8 * y**2 + x * y**1 + 7
-            sp.add(a + b)
+for y in range(9, 18):
+    for x in range(y):
+        a = 5 * 18**3 + x * 18**2 + y * 18 + 10
+        b = 1 * y**3 + 8 * y**2 + x * y + 7
+        sp.add(a + b)
 
 print(len(sp))
