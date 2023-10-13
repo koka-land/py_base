e3a98cb4279e805c6e9b7a124ae153434d77719b
Aleksandr Ermakov <koka-land@yandex.ru>
2023-10-13 15:32:33 +0300

    Update: 13.10.23

--- it-course/lesson_5.py
+++ it-course/lesson_5.py
@@ -21,7 +21,8 @@ def check(string):
             open_bracket = brackets_open[index]
             if stack[-1] == open_bracket:
                 stack = stack[:-1]
-            else: return False
+            else:
+                return False
     return (not stack)
 
 print('1 ', is_correct_brackets('{{[()]}}'), check('{{[()]}}'))
