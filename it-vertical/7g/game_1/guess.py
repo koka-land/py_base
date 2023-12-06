bd4087ab9eb002400ac210fe545c861844b5b9f7
Aleksandr Ermakov <koka-land@yandex.ru>
2023-12-06 12:21:44 +0300

    Update: 06.12.23

--- it-vertical/7g/game_1/guess.py
+++ it-vertical/7g/game_1/guess.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 game = Tk()
 
@@ -9,10 +10,64 @@ x, y = (screen_width/2) - (width/2), (screen_height/2) - (height/2)
 game.geometry('%dx%d+%d+%d' % (width, height, x, y))
 game.title('Угадайка')
 
-label_a = Label(game, text='Label A', bg='yellow', anchor=E)
-label_b = Label(game, text='Label A', bg='yellow', anchor=E)
-opts = {'padx': 50, 'pady': 20, 'fill': BOTH }
-label_a.pack(side=TOP, **opts)
-label_b.place(x=140, y=233)
+title_frame = Frame(game,
+                    bg='#2D1016')
+control_frame = Frame(game,
+                      bg='#FDF1D8')
+title_frame.pack(fill=BOTH,
+                 ipadx=50,)
+control_frame.pack(fill=BOTH,
+                   ipady=10)
+
+button_style = ttk.Style()
+button_style.theme_use('alt')
+button_style.configure("Button.TButton",
+                       font=('PT Sans', 18),
+                       background='#ECD5BB',
+                       foreground='#FF8000',
+                       borderwidth=2)
+button_style.map('Button.TButton',
+                 foreground=[('disabled', '#FF8000'),
+                             ('pressed', '#ECD5BB'),
+                             ('active', '#FDF1D8')],
+                 background=[('disabled', '#123123'),
+                             ('pressed', '!focus', '#ECD5BB'),
+                             ('active', '#FF8000')],
+                 highlightcolor=[('focus', '#2D1016'),
+                                 ('!focus', '#2D1016')],
+                 relief=[('pressed', 'flat'),
+                         ('!pressed', 'flat')])
+
+title_heading = Label(title_frame,
+                      text='УГАДАЙКА',
+                      bg='#2D1016',
+                      fg="#FDF1D8",
+                      font=('PT Sans', 60))
+title_subheading = Label(title_frame,
+                      text='Интелектуальная игра. Не для всех.',
+                      bg='#2D1016',
+                      fg="#FDF1D8",
+                      font=('PT Sans', 18))
+title_heading.pack(fill=BOTH,
+                   pady=(150, 0))
+title_subheading.pack(fill=BOTH,
+                      pady=(0, 150))
+
+button_ai = ttk.Button(control_frame,
+                       text='Загадывает ИИ',
+                       style='Button.TButton')
+button_user = ttk.Button(control_frame,
+                         text='Угадывает ИИ',
+                         style='Button.TButton')
+button_ai.pack(fill=BOTH,
+               side=LEFT,
+               padx=(25, 5),
+               pady=5,
+               expand=True)
+button_user.pack(fill=BOTH,
+                 side=LEFT,
+                 padx=(5, 25),
+                 pady=5,
+                 expand=True)
 
 game.mainloop()
