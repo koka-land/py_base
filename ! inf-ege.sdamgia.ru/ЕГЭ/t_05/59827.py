2e6ce87dfdc2cc3d913812c80b8da78499b2d736
koka-land <koka-land@yandex.ru>
2024-09-17 10:53:11 +0300

    Update 17.09.2024

---
+++ ! inf-ege.sdamgia.ru/ЕГЭ/t_05/59827.py
@@ -0,0 +1,19 @@
+def v3(x):
+    r = ''
+    while x > 0:
+        r = str(x % 3) + r
+        x //= 3
+    return r
+
+max_r = 0
+for n in range(1, 100):
+    r = v3(n)
+    if n % 3 == 0:
+        r = r + r[-2:]
+    else:
+        r  = r + v3((n % 3) * 5)
+    r = int(r, 3)
+    if r <= 173:
+        max_r = max(max_r, r)
+
+print(max_r)
