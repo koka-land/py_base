3ff845dbc4754e46cef156ee2b31382aab7097cf
koka-land <koka-land@yandex.ru>
2025-01-27 11:22:13 +0300

    Update 27.01.2025

---
+++ ! kompege/ЕГЭ/Варианты/ЕКГР 27_04_2024/27B_3.py
@@ -0,0 +1,14 @@
+f = open('files/27_B_3.csv')
+sp = []
+for i in f:
+    sp.append(list(map(float, i.split(';'))))
+
+med = []
+for i in range(len(sp)):
+    s = []
+    x = sp[i][0]
+    y = sp[i][1]
+    for j in range(len(sp)):
+        s.append(((sp[j][0] - x)**2 + (sp[j][1] - y)**2)**0.5)
+    med.append(sum(s) / (len(sp) - 1))
+print(sp[med.index((min(med)))])
---
+++ ! kompege/ЕГЭ/Задания/t_14/19359.py
@@ -0,0 +1,8 @@
+import string
+s = string.digits + string.ascii_lowercase[:12]
+for x in s:
+    a = int('a23' + x + 'ac0', 22)
+    b = int('gb' + x + '21670', 22)
+    c = a + b
+    if c % 21 == 0:
+        print(x, int(x, 22), c // 22)
