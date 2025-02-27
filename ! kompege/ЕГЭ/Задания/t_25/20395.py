4f715e9a7f0ee5e9456539f9fa1aa397b538dd87
koka-land <koka-land@yandex.ru>
2025-02-27 15:09:21 +0300

    Update 27.02.2025

--- ! kompege/ЕГЭ/Задания/t_25/20395.py
+++ ! kompege/ЕГЭ/Задания/t_25/20395.py
@@ -3,7 +3,7 @@ s = '0123456789'
 
 for i in range(2):
     for l in s:
-        for j in product(s, repeat = i):
+        for j in product(s, repeat=i):
             a = int('1' + l + '7301' + ''.join(j) + '320')
             if (a % 8 == 0) and (a % 30 == 0) and (a % 42 == 0):
                 print(a, a // 8)
