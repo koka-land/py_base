17f715283387779abf49154516f411c8324022f4
KoKa <koka-land@yandex.ru>
2026-09-16 17:53:31 +0300

    Update 16.09.26

---
+++ ! kompege/ЕГЭ/Задания/t_26/31520.py
@@ -0,0 +1,19 @@
+f = open('files/26_31520.txt')
+n, p = map(int, f.readline().split())
+c = 0
+a = dict()
+b = []
+for i in f:
+    time, client, size = i.split()
+    time, client, size = list(map(int, time.split(':'))), int(client), int(size)
+    a[client] = a.get(client, 0) + size
+    if time[0] < 12:
+        if c + size <= p:
+            c += size
+        else:
+            b.append(c)
+            c = size
+b.sort(reverse=True)
+print(max(a, key=a.get))
+print(b[0] + b[1])
+print(b[0])
